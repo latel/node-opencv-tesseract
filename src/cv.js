8514007086bd06fff5075b513e7dcc47ba25ac90
okwang <okwang@jintengtech.com>
2024-11-28 18:27:02 +0800

    update

--- src/cv.js
+++
@@ -1,4 +0,0 @@
-import { createWorker } from 'tesseract.js';
-import cv from 'opencv.js';
-import fs from 'fs';
-import { createCanvas, loadImage } from 'canvas';
--- src/index.js
+++
@@ -1,4 +0,0 @@
-const mqtt = require('mqtt');
-const cv = require('opencv4nodejs');
-const Tesseract = require('tesseract.js');
-const { promisify } = require('util');
